c6e904fc6d87a1c7cb84cbbc804b8d0c7e1bbe08
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:28:54 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "storage_containers" {
+  description = <<EOT
+Map of storage_containers, attributes below
+Required:
+    - name
+Optional:
+    - container_access_type
+    - default_encryption_scope
+    - encryption_scope_override_enabled
+    - metadata
+    - storage_account_id
+    - storage_account_name
+EOT
+
+  type = map(object({
+    name                              = string
+    container_access_type             = optional(string, "private")
+    default_encryption_scope          = optional(string)
+    encryption_scope_override_enabled = optional(bool, true)
+    metadata                          = optional(map(string))
+    storage_account_id                = optional(string)
+    storage_account_name              = optional(string)
+  }))
+}
+
